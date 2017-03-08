4230128f62bdfb82f49b6771d7c3cbba067948a0
MichaelWiss <michael.wiss@gmail.com>
2017-03-07 18:16:37 -0600

    middleware

--- server/server.js
+++ server/server.js
@@ -3,3 +3,9 @@ const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
