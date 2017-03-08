3cc49709dcf7f234f2c892210f3607ce0adedf7b
MichaelWiss <michael.wiss@gmail.com>
2017-03-08 15:48:38 -0600

    socket io

--- server/server.js
+++ server/server.js
@@ -7,9 +7,10 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log('Server is up on port ${port');
 });
