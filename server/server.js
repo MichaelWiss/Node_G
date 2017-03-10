a5543dab40bd5b47344804cc38d251dc2c5a79ae
MichaelWiss <michael.wiss@gmail.com>
2017-03-09 20:40:17 -0600

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -17,26 +17,18 @@ io.on('connection', (socket) => {
    console.log('New user connected');
 
    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-   });
+   
 
-   socket.broadcast.emit('newMessage', {
-   	from: 'Admin',
-   	text: 'New User joined',
-   	 createdAt: new Date().getTime()
-   });
+   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
    socket.on('createMessage', (message) => {
       console.log('createMessage', message);
-      // io.emit('newMessage', {
+      io.emit('newMessage', generateMessage(message.from, message.text));
+      // socket.broadcast.emit('newMessage', {
       // 	from: message.from,
       // 	text: message.text,
       // 	createdAt: new Date().getTime()
       // });
-      socket.broadcast.emit('newMessage', {
-      	from: message.from,
-      	text: message.text,
-      	createdAt: new Date().getTime()
-      });
    });
 
    socket.on('disconnect', () => {
