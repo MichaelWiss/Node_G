2c6de62a5cfb47bbbf2e5860b75639d3d5f4195a
MichaelWiss <michael.wiss@gmail.com>
2017-03-09 20:05:19 -0600

    message object

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const{generateMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,10 +16,7 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
    console.log('New user connected');
 
-   socket.emit('newMessage', {
-   	  from: 'Admin',
-   	  text: 'Welcome to the chat app',
-   	  createdAt: new Date().getTime()
+   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
    });
 
    socket.broadcast.emit('newMessage', {
