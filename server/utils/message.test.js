4ed1785f0db300ebf4d118e0fcd15ae91a1b9ae7
MichaelWiss <michael.wiss@gmail.com>
2017-03-14 13:27:57 -0500

    test geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -15,6 +15,10 @@ describe('generateMessage', () => {
 
 describe('generateLocationMessage', () => {
 	it('should generate correct location object', () =>
-		
+		var from = 'Deb';
+		var latitude = 15;
+		var longitude = 19;
+		var url = 'https://www.google.com/maps?=15,19';
+		var message = generateLocationMessage()
    });
 });
