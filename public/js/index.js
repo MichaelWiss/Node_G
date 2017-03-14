4de7950aef9a36bbda104205d353f9d002e50c33
MichaelWiss <michael.wiss@gmail.com>
2017-03-13 21:51:51 -0500

    geolocation coords emitted

--- public/js/index.js
+++ public/js/index.js
@@ -42,7 +42,10 @@ locationButton.on('click', function () {
    }
 
    navigator.geolocation.getCurrentPosition(function (position) {
-   	console.log(position);
+   	socket.emit('createLocationMessage', {
+   		latitude: position.coords.latitude,
+   		longitude: position.coords.longitude
+   	});
    }, function () {
       alert('Unable to fetch location.');
    });
