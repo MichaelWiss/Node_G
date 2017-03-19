3bc59a4e640b3982b172533c581436d90f6ce042
MichaelWiss <michael.wiss@gmail.com>
2017-03-19 17:49:42 -0500

    new user validation

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const{generateMessage, generateLocationMessage} = require('./utils/message');
-
+const{isRealString} = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -22,7 +22,9 @@ io.on('connection', (socket) => {
    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
    socket.on('join', (params, callback) => {
-       
+       if (!isRealString(params.name) || !isRealString(params.room)){
+         
+       }
    });
 
    socket.on('createMessage', (message, callback) => {
