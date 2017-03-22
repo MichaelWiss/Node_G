82b6033ecc4a0ef814b0a44a8017cf92fa05cc42
MichaelWiss <michael.wiss@gmail.com>
2017-03-21 20:08:58 -0500

    classs for users

--- server/utils/users.js
+++ server/utils/users.js
@@ -13,8 +13,10 @@ class Users {
 	constructor () {
 		this.users = [];
 	}
-	addUser () {
-		
+	addUser (id, name, room) {
+		var user = {id, name, room};
+		this.users.push(user);
+        return user;
 	}
 
 }
