f91a0922daabdc787613f3aeb93deb47af7fe74e
MichaelWiss <michael.wiss@gmail.com>
2017-03-22 09:17:55 -0500

    users test

--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -1,3 +1,10 @@
 const expect = require('expect');
 
 const {Users} = require('./users');
+
+describe('Users', () => {
+   
+   it('should add new user', () => {
+   	var users = new Users();
+   })
+});
