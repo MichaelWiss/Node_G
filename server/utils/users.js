6cbd05524bd8db47d5147433fe3a006b7ac1c1a3
MichaelWiss <michael.wiss@gmail.com>
2017-03-21 19:58:48 -0500

    Users class

--- server/utils/users.js
+++ server/utils/users.js
@@ -9,17 +9,27 @@
 // getUser(id)
 // getUserList(room)
 
-class Person {
-   constructor (name, age) {
-     console.log(name, age);
-     this.name = name;
-     this.age = age;
-   }
-   getUserDescription() {
-   	return `${this.name} is ${this.age} old.`;
-   }
+class Users {
+	constructor () {
+		this.users = [];
+	}
+	addUser () {
+		
+	}
+
 }
 
-var me = new Person("michael", 30);
-var description = me.getUserDescription();
-console.log(description);
+// class Person {
+//    constructor (name, age) {
+//      console.log(name, age);
+//      this.name = name;
+//      this.age = age;
+//    }
+//    getUserDescription() {
+//    	return `${this.name} is ${this.age} old.`;
+//    }
+// }
+
+// var me = new Person("michael", 30);
+// var description = me.getUserDescription();
+// console.log(description);
