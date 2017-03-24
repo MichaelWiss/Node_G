5186d3ad6f43de0aabb5a597d2f1af141ba5c752
MichaelWiss <michael.wiss@gmail.com>
2017-03-24 16:00:14 -0500

    removeUser

--- server/utils/users.js
+++ server/utils/users.js
@@ -19,7 +19,13 @@ class Users {
         return user;
 	}
 	removeUser(id) {
-		// return user that was removed
+		var user = this.getUser(id);
+
+		if (user) {
+           this.users = this.user.filter((user) => user.id !== id);
+		}
+		
+		return user;
 	}
 	getUser (id) {
        return this.users.filter((user)  => user.id === id)[0]
