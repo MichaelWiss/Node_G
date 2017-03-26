ff25991544497cfa4c4c318b44bbe7b4ff991c36
MichaelWiss <michael.wiss@gmail.com>
2017-03-26 14:11:20 -0500

    users list

--- public/js/chat.js
+++ public/js/chat.js
@@ -35,7 +35,12 @@ var socket = io();
  });
 
 socket.on('updateUserList', function (users) {
-  console.log('Users list', users);
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+  jQuery('#users').html(ol);
 });
 
  socket.on('newMessage', function (message) {
