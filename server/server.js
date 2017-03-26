00f42e21e12d19aa343d4d0e5d7253725baa440f
MichaelWiss <michael.wiss@gmail.com>
2017-03-26 14:18:51 -0500

    server.js

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ io.on('connection', (socket) => {
 
       if (user) {
          io.to(user.room).emit('updatUserList', users.getUserList(user.room));
-         io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+         io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left));
       }
    	
    });
